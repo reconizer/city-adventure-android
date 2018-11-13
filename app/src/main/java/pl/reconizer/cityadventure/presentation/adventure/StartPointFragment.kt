ed64a7a9f1819c2b6d4be8a3bba41726ada370be
Lukasz Kielczykowski <lukasz.kielczykowski@tokagen.com>
2018-11-13 15:52:03 +0100

    Adds stamp and rating animation

--- app/src/main/java/pl/reconizer/cityadventure/presentation/adventure/StartPointFragment.kt
+++ app/src/main/java/pl/reconizer/cityadventure/presentation/adventure/StartPointFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.core.os.bundleOf
+import com.bartoszlipinski.viewpropertyobjectanimator.ViewPropertyObjectAnimator
 import kotlinx.android.synthetic.main.fragment_adventure_start_point.*
+import kotlinx.android.synthetic.main.view_adventure_start_point_rating.*
 import pl.reconizer.cityadventure.R
 import pl.reconizer.cityadventure.domain.entities.Adventure
 import pl.reconizer.cityadventure.presentation.common.BaseFragment
@@ -27,10 +30,58 @@ class StartPointFragment : BaseFragment() {
 
         authorInfo.name = "Cool Author"
         authorInfo.setLogo("https://placekitten.com/g/300/300")
+
+        ratingView.rateListener = {
+            showRating()
+        }
+        ratingStamp.alpha = 0f
+
+        closeButton.setOnClickListener {
+            navigator.goBack()
+        }
+    }
+
+    private fun showRating() {
+        ratingStamp.rating = ratingView.rating
+        ViewPropertyObjectAnimator
+                .animate(ratingViewContainer)
+                .withLayer()
+                .alpha(0f)
+                .setInterpolator(AccelerateDecelerateInterpolator())
+                .setDuration(RATING_VISIBILITY_ANIMATION_DURATION)
+                .setStartDelay(RATING_ANIMATION_DELAY)
+                .start()
+        ViewPropertyObjectAnimator
+                .animate(ratingViewContainer)
+                .withLayer()
+                .height(0)
+                .setInterpolator(AccelerateDecelerateInterpolator())
+                .setDuration(RATING_HIGHT_ANIMATION_DURATION)
+                .setStartDelay(RATING_VISIBILITY_ANIMATION_DURATION + RATING_ANIMATION_DELAY)
+                .start()
+        showRatingStamp()
+    }
+
+    private fun showRatingStamp() {
+        val scale = 1.5f
+        ratingStamp.scaleX = scale
+        ratingStamp.scaleY = scale
+        ViewPropertyObjectAnimator
+                .animate(ratingStamp)
+                .scales(1f)
+                .alpha(1f)
+                .setInterpolator(AccelerateDecelerateInterpolator())
+                .setDuration(RATING_STAMP_ANIMATION_DURATION)
+                .setStartDelay(RATING_VISIBILITY_ANIMATION_DURATION + RATING_HIGHT_ANIMATION_DURATION + RATING_ANIMATION_DELAY)
+                .start()
     }
 
     companion object {
         const val ADVENTURE_PARAM = "adventure"
+        const val RATING_ANIMATION_DELAY = 300L
+        const val RATING_VISIBILITY_ANIMATION_DURATION = 200L
+        const val RATING_HIGHT_ANIMATION_DURATION = 200L
+        const val RATING_STAMP_ANIMATION_DURATION = 500L
 
         fun newInstance(adventure: Adventure): StartPointFragment {
             return StartPointFragment().apply {
