6b5df1d4e028f120a49a57e088791ed200c7e59e
Lukasz Kielczykowski <kielczykowski.lukasz@gmail.com>
2019-01-16 12:52:53 +0100

    Changes error dialog for pretty one

--- app/src/main/java/pl/reconizer/cityadventure/presentation/common/BaseFragment.kt
+++ app/src/main/java/pl/reconizer/cityadventure/presentation/common/BaseFragment.kt
@@ -2,18 +2,17 @@ package pl.reconizer.cityadventure.presentation.common
 
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import pl.reconizer.cityadventure.MainActivity
-import pl.reconizer.cityadventure.R
 import pl.reconizer.cityadventure.data.entities.Error
 import pl.reconizer.cityadventure.presentation.authentication.login.LoginFragment
+import pl.reconizer.cityadventure.presentation.customviews.PrettyDialog
 import pl.reconizer.cityadventure.presentation.mvp.IView
 import pl.reconizer.cityadventure.presentation.navigation.INavigator
 
 open class BaseFragment : Fragment(), IView {
 
-    private var genericErrorDialog: AlertDialog? = null
+    private var errorDialog: PrettyDialog? = null
 
     val navigator: INavigator
         get() = (activity as MainActivity).navigator
@@ -29,14 +28,9 @@ open class BaseFragment : Fragment(), IView {
 
     override fun showGenericError() {
         context?.let {
-            if (genericErrorDialog == null) {
-                genericErrorDialog = AlertDialog.Builder(it).run {
-                    setMessage(R.string.error_something_went_wrong)
-                    setNeutralButton(R.string.common_close) { dialog, _ -> dialog.dismiss() }
-                    create()
-                }
-            }
-            if (!genericErrorDialog!!.isShowing) { genericErrorDialog!!.show() }
+            errorDialog?.dismiss()
+            errorDialog = PrettyDialog.ErrorDialogBuilder(it).build()
+            errorDialog?.show(childFragmentManager, "error")
         }
     }
 
