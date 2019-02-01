68c46059cf434fa47ef38bc54f79cba339ddba8c
Łukasz Kiełczykowski <kielczykowski.lukasz@gmail.com>
2019-02-01 16:16:14 +0100

    Changes image clue format

--- app/src/main/java/pl/reconizer/cityadventure/presentation/adventure/journal/clues/ImageClueViewHolder.kt
+++ app/src/main/java/pl/reconizer/cityadventure/presentation/adventure/journal/clues/ImageClueViewHolder.kt
@@ -2,6 +2,8 @@ package pl.reconizer.cityadventure.presentation.adventure.journal.clues
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.isGone
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.view_journal_clue_with_image.view.*
 import pl.reconizer.cityadventure.domain.entities.Clue
@@ -9,9 +11,12 @@ import pl.reconizer.cityadventure.domain.entities.Clue
 class ImageClueViewHolder(view: View) : ClueViewHolder(view) {
 
     val image: ImageView = view.content
+    val content: TextView = view.contentText
 
     override fun bind(clue: Clue, idx: Int) {
         super.bind(clue, idx)
+        content.text = clue.description
+        content.isGone = clue.description.isNullOrEmpty()
         Picasso.get()
                 .load(clue.originalResourceUrl)
                 .into(image)
