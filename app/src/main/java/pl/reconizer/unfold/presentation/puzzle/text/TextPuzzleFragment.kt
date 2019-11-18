9b63f6c10977ed30afa589fa13b48d3854f6e7ef
Lukasz Kielczykowski <kielczykowski.lukasz@gmail.com>
2019-11-18 10:20:11 +0100

    Removes unnecessary imports

--- app/src/main/java/pl/reconizer/unfold/presentation/puzzle/text/TextPuzzleFragment.kt
+++ app/src/main/java/pl/reconizer/unfold/presentation/puzzle/text/TextPuzzleFragment.kt
@@ -1,19 +1,15 @@
 package pl.reconizer.unfold.presentation.puzzle.text
 
 import android.os.Bundle
-import android.text.Editable
 import android.text.InputType
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.EditText
 import kotlinx.android.synthetic.main.fragment_text_puzzle.*
 import pl.reconizer.unfold.R
 import pl.reconizer.unfold.common.extensions.hideKeyboard
 import pl.reconizer.unfold.common.extensions.showKeyboard
-import pl.reconizer.unfold.presentation.common.SimpleTextWatcherAdapter
 import pl.reconizer.unfold.presentation.puzzle.BasePuzzleFragment
 
 class TextPuzzleFragment : BasePuzzleFragment() {
